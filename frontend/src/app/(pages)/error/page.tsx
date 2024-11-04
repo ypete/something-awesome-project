db2264f6405f8cbb1da6d80102746656eee3ba0f
Peter Yang <z5476245@ad.unsw.edu.au>
2024-11-04 11:15:30 +1100

    Resolved useSearchParams error

--- frontend/src/app/(pages)/error/page.tsx
+++ frontend/src/app/(pages)/error/page.tsx
@@ -6,21 +6,27 @@ import Nav from "@/app/components/Nav";
 import Footer from '@/app/components/Footer';
 import { Suspense } from 'react';
 
-export default function Error() {
+const ErrorContent = () => {
   const searchParams = useSearchParams();
   const errorMessage = searchParams.get("message") || "Something went wrong";
 
   return (
+    <div className="relative flex flex-col font-bold items-center min-h-screen mt-36">
+      <h2 className="bg-gradient-to-r from-red-100 to-red-300 rounded w-max text-4xl px-4 py-1 my-2 text-white">ERROR</h2>
+      <h1 className="text-7xl text-red-500 mt-2 mb-4">{errorMessage}</h1>
+      <h1 className="text-3xl">That&apos;s all we know.</h1>
+    </div>
+  );
+};
+
+export default function Error() {
+  return (
     <>
-      <Nav/>
+      <Nav />
       <Suspense fallback={<div>Loading...</div>}>
-        <div className="relative flex flex-col font-bold items-center min-h-screen mt-36">
-          <h2 className="bg-gradient-to-r from-red-100 to-red-300 rounded w-max text-4xl px-4 py-1 my-2 text-white">ERROR</h2>
-          <h1 className="text-7xl text-red-500 mt-2 mb-4">{errorMessage}</h1>
-          <h1 className="text-3xl">That&apos;s all we know.</h1>
-        </div>
+        <ErrorContent />
       </Suspense>
-      <Footer/>
+      <Footer />
     </>
   );
 }
